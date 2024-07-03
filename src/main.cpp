0d7484ba0a877ac5067207df7b9ece4d985b648f
Konvt <120050092+Konvt@users.noreply.github.com>
2024-07-03 12:46:32 +0800

    a few minor adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 #include "Shell.hpp"
 using namespace std;
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
   if ( argc == 0 )
     abort();
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
     return simsh::shell::Shell().run();
 
   if ( "-c"sv == argv[1] || argc > 2 ) {
-    if (  argc == 2 && "-c"sv == argv[1] ) {
+    if ( argc == 2 && "-c"sv == argv[1] ) {
       simsh::iout::logger << simsh::error::ArgumentError(
         "simsh: -c:", "option requires an argument"
       );
@@ -30,35 +30,21 @@ int main(int argc, char **argv)
     }
 
     simsh::utils::Pipe pipe;
-    if ( auto process_id = fork();
-         process_id < 0 ) {
-      simsh::iout::logger.print( simsh::error::SystemCallError( "fork" ) );
-      return EXIT_FAILURE;
-    } else if ( process_id == 0 ) {
-      pipe.writer().close();
-      dup2( pipe.reader().get(), STDIN_FILENO );
-      return simsh::shell::BaseShell().run();
-    } else {
-      const auto args = "-c"sv == argv[1] ? span( argv + 2, argc - 2 ) : span( argv + 1, argc - 1 );
-      for ( auto e : args ) {
-        pipe.writer().push( e );
-        pipe.writer().push( ' ' );
-      }
-      pipe.writer().push( '\n' );
-      pipe.writer().close();
-
-      int status;
-      if ( waitpid( process_id, &status, 0 ) < 0 ) {
-        simsh::iout::logger.print( simsh::error::SystemCallError( "waitpid" ) );
-        return EXIT_FAILURE;
-      }
-      return WEXITSTATUS( status );
+    dup2( pipe.reader().get(), STDIN_FILENO );
+    const auto args = "-c"sv == argv[1]
+      ? span( argv + 2, argc - 2 ) : span( argv + 1, argc - 1 );
+    for ( auto e : args ) {
+      pipe.writer().push( e );
+      pipe.writer().push( ' ' );
     }
+    pipe.writer().push( '\n' );
+    pipe.writer().close();
+    return simsh::shell::BaseShell().run();
   } else {
     ifstream ifs { argv[1] };
     if ( !ifs ) {
-      simsh::iout::logger << simsh::error::SystemCallError(
-        format( "{}: {}", argv[1], "could not open the file" ) );
+      simsh::iout::logger.print( simsh::error::SystemCallError(
+        format("simsh: {}", argv[1] ) ) );
       return EXIT_FAILURE;
     }
     return simsh::shell::BaseShell( simsh::Parser( ifs ) ).run();
